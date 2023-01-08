36f606c358921cca4adae05f5a655eb6caf0423a
Dianciré Diallo <diaancir@gmail.com>
2023-01-08 16:58:55 +0000

    Organization of components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experiences from './components/Experiences/Experiences'
+import Portofolio from './components/Portofolio/Portofolio'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import './styles/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Nav />
+    <About />
+    <Experiences />
+    <Portofolio />
+    <Contact />
+    <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
